6e00fa21193969310ecfa643833db2be9fa7f594
Jayson <96589775+Jayson-Amike@users.noreply.github.com>
2025-05-30 01:14:52 -0400

    mudating nav bar with logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
+import logo from '../images/logo.png'; // adjust path as needed
+
 
 const Navbar = () => {
   const location = useLocation();
@@ -15,17 +17,15 @@ const Navbar = () => {
           <div className="flex items-center">
             <Link
               to="/"
-              className="text-xl font-bold text-food-green-700 flex items-center"
+              className="text-2xl font-extrabold   flex items-center"
               onClick={() => setIsMenuOpen(false)}
             >
-              <svg
-                className="w-8 h-8 mr-2 text-food-orange-500"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
-              </svg>
-              Company
+                <img
+    src={logo}
+    alt="Company Logo"
+    className="h-14 w-auto object-contain mr-3"
+  />
+              Open Doors
             </Link>
           </div>
 
